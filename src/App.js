cf63eb0bb62d729bbd272975fc2396f968440e7b
mnoorcahyo <mnoorcahyo@gmail.com>
2018-11-15 17:25:40 +0700

    css todoapp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Todos from './Todos'
 import AddTodo from './AddTodo';
+import './App.css'
 
 class App extends Component {
   state = {
@@ -30,9 +31,11 @@ class App extends Component {
   render() {
     return (
       <div className="todo-app container">
+        <div className="todo-wrapper">
         <h1 className="center blue-text"> Todo's App</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
         <AddTodo addTodo={this.addTodo}/>
+        </div>
       </div>
     );
   }
--- src/Todos.js
+++ src/Todos.js
@@ -5,8 +5,9 @@ const Todos = ({todos, deleteTodo}) => {
     const todoList = todos.length ? (
         todos.map(todo =>{
             return (
-                <div className="collection-item" key={todo.id}>
-                    <span onClick={()=>{deleteTodo(todo.id)}}>{todo.content}</span>
+                <div className="collection-item listTodo" key={todo.id}>
+                    <span>{todo.content}</span>
+                    <span onClick={()=>{deleteTodo(todo.id)}}>X</span>
                 </div>
             )
         })
