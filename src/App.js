f2d0b5cfffe943ba0a78a915da01ccb824049bb3
mnoorcahyo <mnoorcahyo@gmail.com>
2018-11-15 12:54:38 +0700

    first todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos'
 
 class App extends Component {
+  state = {
+    todos : [
+      {id:1, content: 'first content'},
+      {id:2, content: 'second content'}
+    ]
+  }
+
+  deleteTodo = (id) =>{
+    let todos = this.state.todos.filter(todo =>{
+      return todo.id !== id
+    })
+    this.setState({
+      todos:todos
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text"> Todo's App</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
       </div>
     );
   }
