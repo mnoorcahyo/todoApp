48f4aff0ef748ae7384d46dfd3d9a0b09184dd9b
mnoorcahyo <mnoorcahyo@gmail.com>
2018-11-16 07:07:39 +0700

    add Flip Move

--- src/Todos.js
+++ src/Todos.js
@@ -1,24 +1,32 @@
-import React from 'react';
+import React from "react";
+import FlipMove from "react-flip-move";
 
-const Todos = ({todos, deleteTodo}) => {
-    
-    const todoList = todos.length ? (
-        todos.map(todo =>{
-            return (
-                <div className="collection-item listTodo" key={todo.id}>
-                    <span>{todo.content}</span>
-                    <span onClick={()=>{deleteTodo(todo.id)}}>X</span>
-                </div>
-            )
-        })
-    ) : (
-        <p className="center"> You have no todo's left, yay!</p>
-    )
-    return(
-        <div className="todos collection">
-            {todoList}
+const Todos = ({ todos, deleteTodo }) => {
+  const todoList = todos.length ? (
+    todos.map(todo => {
+      return (
+        <div className="collection-item listTodo" key={todo.id}>
+          <span>{todo.content}</span>
+          <span
+            onClick={() => {
+              deleteTodo(todo.id);
+            }}
+          >
+            X
+          </span>
         </div>
-    )
-}
+      );
+    })
+  ) : (
+    <p className="center"> You haven't todo's !!</p>
+  );
+  return (
+    <div className="todos collection">
+      <FlipMove duration={250} easing="ease-out">
+        {todoList}
+      </FlipMove>
+    </div>
+  );
+};
 
-export default Todos
+export default Todos;
